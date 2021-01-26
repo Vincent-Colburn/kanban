7da13b672293394795dde5766e067564c78e66e2
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-01-25 17:21:20 -0700

    Things work, api works, and stuff that works

--- kanban.server/server/controllers/BoardController.js
+++ kanban.server/server/controllers/BoardController.js
@@ -6,16 +6,51 @@ export class BoardController extends BaseController {
   constructor() {
     super('api/boards')
     this.router
-      // .get('', this.getAll)
-      // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
       .use(Auth0Provider.getAuthorizedUserInfo)
+      .get('', this.getAll)
+      .get(':boardId', this.getOne)
       .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.delete)
   }
 
   async create(req, res, next) {
     try {
       req.body.creatorId = req.userInfo.id
       res.send(await boardService.create(req.body))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  // ANCHOR why is it req.query
+  async getAll(req, res, next) {
+    try {
+      return res.send(await boardService.getAll(req.query))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      return res.send(await boardService.delete(req.params.id))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getOne(req, res, next) {
+    try {
+      res.send(await boardService.getOne(req.params.boardId))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      res.send(await boardService.edit(req.params.id, req.body))
     } catch (error) {
       next(error)
     }
--- kanban.server/server/services/BoardService.js
+++ kanban.server/server/services/BoardService.js
@@ -1,9 +1,35 @@
 import { dbContext } from '../db/DbContext'
-// import { BadRequest } from '../utils/Errors'
+import { BadRequest } from '../utils/Errors'
 
 class BoardService {
+  async getOne(boardId) {
+    const boardFound = await dbContext.Boards.findById(boardId)
+    if (!boardFound) {
+      throw new BadRequest('No Board exists with that ID')
+    }
+    return boardFound
+  }
+
+  async delete(id) {
+    return await dbContext.Boards.findByIdAndDelete(id)
+  }
+
   async create(body) {
     return await dbContext.Boards.create(body)
+  }
+
+  async getAll(query) {
+    return await dbContext.Boards.find(query)
+  }
+
+  async edit(id, title) {
+    // NOTE why was this getone here
+    // await this.getOne(id)
+    const update = await dbContext.Boards.findByIdAndUpdate(id, title, { new: true })
+    if (!update) {
+      throw new BadRequest('No Board exists with that ID')
+    }
+    return update
   }
 }
 
