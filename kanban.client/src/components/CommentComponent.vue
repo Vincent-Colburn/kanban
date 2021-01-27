5d003256f08cf09103bf000f55b84f327f66bd28
Vincent-Girard <vincent.r.girard@gmail.com>
2021-01-27 16:46:57 -0700

    16/17 requirements, comments have all functionality

--- kanban.client/src/components/CommentComponent.vue
+++ kanban.client/src/components/CommentComponent.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="component">
     <p>{{ commentProps.creator.name }}  :  {{ commentProps.body }}</p>
+    <i class="fa fa-trash text-danger" v-if="state.account.id == commentProps.creatorId" @click="deleteComment" aria-hidden="true"></i>
   </div>
 </template>
 
 <script>
+import { computed, reactive } from 'vue'
+import { commentService } from '../services/CommentService'
+import { logger } from '../utils/Logger'
+import { AppState } from '../AppState'
 export default {
   name: 'CommentComponent',
   props: {
     commentProps: { type: Object, required: true }
   },
-  setup() {
-    return {}
+  setup(props) {
+    const state = reactive({
+      account: computed(() => AppState.account)
+
+    })
+    return {
+      state,
+      deleteComment() {
+        try {
+          console.log('this is the delete comment from component', props.commentProps.task)
+          commentService.deleteComment(props.commentProps)
+        } catch (error) {
+          logger.error(error)
+        }
+      }
+    }
   },
   components: {}
 }
--- kanban.client/src/components/TaskComponent.vue
+++ kanban.client/src/components/TaskComponent.vue
@@ -2,6 +2,25 @@
   <div class="component col-12">
     <h3>{{ taskProp.title }} </h3>
     <i class="fa fa-trash text-danger" v-if="state.account.id == taskProp.creatorId" @click="deleteTask" aria-hidden="true"></i>
+    <form action="form-inline border justify-content-center align-items-center" @submit.prevent="createComment">
+      <p>Create Comment</p>
+      <input
+        type="text"
+        name="body"
+        id="body"
+        v-model="state.newComment.body"
+        class="form-control d-flex"
+        placeholder="Enter new comment here...."
+        aria-describedby="title"
+      />
+      <button type="submit" class="btn btn-success">
+        +
+      </button>
+    </form>
+
+    <!--  -->
+    <!-- <label for="tasks"> Choose a new List to put this task on!</label>
+    <select name="lists" id=""> -->
   </div>
   <div class="col-12">
     <CommentComponent v-for="comment in state.comments" :key="comment.id" :comment-props="comment" />
@@ -22,7 +41,11 @@ export default {
   setup(props) {
     const state = reactive({
       account: computed(() => AppState.account),
-      comments: computed(() => AppState.comments[props.taskProp.id])
+      comments: computed(() => AppState.comments[props.taskProp.id]),
+      newComment: {
+        body: '',
+        task: props.taskProp.id
+      }
     })
     onMounted(async() => {
       try {
@@ -36,6 +59,14 @@ export default {
       deleteTask() {
         try {
           taskService.deleteTask(props.taskProp)
+        } catch (error) {
+          logger.log(error)
+        }
+      },
+      createComment() {
+        try {
+          commentService.createComment(state.newComment)
+          state.newComment.body = ''
         } catch (error) {
           logger.log(error)
         }
